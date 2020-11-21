0477b6aab29cf7a311d3044d808d31cda8a4eebc
kusumkoli <kusumkoli372@gmail.com>
2020-11-21 21:56:36 +0530

    login/signup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
 class Login extends Component {
   constructor() {
     super();
@@ -11,29 +13,38 @@ class Login extends Component {
       errors: {}
     };
   }
-  onChange = e => {
-      this.setState({ [e.target.id]: e.target.value });
-  };
 
-  onSubmit = async e => {
-      e.preventDefault();
-      const userData = {
-        email: this.state.email,
-        password: this.state.password
-      };
-
-      try {
-        const response = await axios.post('http://localhost:5000/api/users/login', userData);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
   
+componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard"); // push user to dashboard when they login
+    }
+if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
   };
-
-  render() {
-      const { errors } = this.state;
-  return (
+onSubmit = e => {
+    e.preventDefault();
+const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+  };
+render() {
+    const { errors } = this.state;
+return (
       <div className="container">
         <div style={{ marginTop: "4rem" }} className="row">
           <div className="col s8 offset-s2">
@@ -57,8 +68,15 @@ class Login extends Component {
                   error={errors.email}
                   id="email"
                   type="email"
+                  className={classnames("", {
+                    invalid: errors.email || errors.emailnotfound
+                  })}
                 />
                 <label htmlFor="email">Email</label>
+                <span className="red-text">
+                  {errors.email}
+                  {errors.emailnotfound}
+                </span>
               </div>
               <div className="input-field col s12">
                 <input
@@ -67,8 +85,15 @@ class Login extends Component {
                   error={errors.password}
                   id="password"
                   type="password"
+                  className={classnames("", {
+                    invalid: errors.password || errors.passwordincorrect
+                  })}
                 />
                 <label htmlFor="password">Password</label>
+                <span className="red-text">
+                  {errors.password}
+                  {errors.passwordincorrect}
+                </span>
               </div>
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                 <button
@@ -91,5 +116,16 @@ class Login extends Component {
     );
   }
 }
-
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+
 const initialState = {};
 const middleware = [thunk];
+
 const store = createStore(
   rootReducer,
   initialState,
@@ -15,5 +17,3 @@ const store = createStore(
   )
 );
 export default store;
-
-
